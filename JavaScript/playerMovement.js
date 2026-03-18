69c9a31e1767f947ad178ab62cd20e31858b03b4
Pierre <pierre.bourgeoisdelavergne@ynov.com>
2026-03-18 14:21:37 +0100

    S'arrêter face à un ennemi + réparation du bug de duplication du joueur

--- JavaScript/playerMovement.js
+++ JavaScript/playerMovement.js
@@ -1,3 +1,8 @@
+
+/* -------------------------------------------- */
+/*        Touches de clavier pressées           */
+/* -------------------------------------------- */
+
 document.addEventListener('keyup', (e) => {
     let moveX = Player.x;
     let moveY = Player.y;
@@ -10,6 +15,8 @@ document.addEventListener('keyup', (e) => {
         moveX -= 1;
     } else if (e.key === "d" || e.key === "ArrowRight") {
         moveX += 1;
+    } else {
+        return;
     }
 
     moveOnMap(moveX, moveY);
@@ -19,40 +26,60 @@ document.addEventListener('keyup', (e) => {
 /*            Bouger sur la carte               */
 /* -------------------------------------------- */
 
-let originTile = CELL_TYPES.FLOOR;;
+let originTile = CELL_TYPES.FLOOR;
 
-function moveOnMap (x, y) {
-    if (y >= 0 && y < map.length && x >= 0 && x < map[y].length && map[y][x] !== CELL_TYPES.WALL && map[y][x] !== CELL_TYPES.ENNEMY) {
-        
-        // Sauvegarde de la tuile de destination
-        let nextTile = map[y][x];
+function moveOnMap(x, y) {
 
-        // Remettre la tuile d'origine
-        map[Player.y][Player.x] = originTile;
-
-        // Enregistrer prochaine destination
-        originTile = nextTile;
-
-        // Afficher une notif si c'est un coffre via l'orverlay message
-        if (map[y][x] === CELL_TYPES.CHEST) {
-            // Gagnés dans le coffre
-            giveItems({gold: 10, xp:20, potion_vie:1});
-        }
-
-        // Afficher une notif si c'est un coffre via l'orverlay message
-        if (map[y][x] === CELL_TYPES.ENNEMY) {
-            printMessage("Vous êtes tombé sur un ennemi")
-        }
-
-        // Dépacer joueur
-        map[y][x] = CELL_TYPES.PLAYER;
-
-        // MAJ position du joueur
-        Player.x = x;
-        Player.y = y;
-
-        // Afficher la carte mise à jour
-        loadMap();
+    // Vérifier si on sort des limites
+    if (y < 0 || y >= map.length || x < 0 || x >= map[y].length) {
+        return;
     }
+
+    /* -------------------------------------------- */
+
+    // Afficher une notif si c'est un ennemi via l'orverlay message
+    if (map[y][x] === CELL_TYPES.ENNEMY) {
+        printMessage("Vous êtes tombé sur un ennemi");
+        return; // Ne pas bouger
+    }
+
+    /* -------------------------------------------- */
+
+
+    // S'arrêter si c'est un mur
+    if (map[y][x] === CELL_TYPES.WALL) {
+        return;
+    }
+
+    /* -------------------------------------------- */
+
+    // Sauvegarde de la tuile de destination
+    let nextTile = map[y][x];
+
+    // Remettre la tuile d'origine
+    map[Player.y][Player.x] = originTile;
+
+    // Enregistrer prochaine destination
+    originTile = nextTile;
+
+    /* -------------------------------------------- */
+
+    // Afficher une notif si c'est un coffre via l'orverlay message
+    if (nextTile === CELL_TYPES.CHEST) {
+        // Gagnés dans le coffre
+        giveItems({ gold: 10, xp: 20, potion_vie: 1 });
+    }
+
+    /* -------------------------------------------- */
+
+    // Dépacer joueur
+    map[y][x] = CELL_TYPES.PLAYER;
+
+    // MAJ position du joueur
+    Player.x = x;
+    Player.y = y;
+
+    // Afficher la carte mise à jour
+    loadMap();
 }
 
