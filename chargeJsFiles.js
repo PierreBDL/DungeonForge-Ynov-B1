dc04fa1e98af9a0507c276cba552054dcee98e21
Pierre <pierre.bourgeoisdelavergne@ynov.com>
2026-03-21 18:24:31 +0100

    Chargement des scripts grâce à un seul fichier js afin de nettoyer le html

---
+++ chargeJsFiles.js
@@ -0,0 +1,28 @@
+// Permet de charger les scripts JS sans avoir à le faire manuellement dans le HTML
+
+let scriptsFiles = [
+    "mobs.js",
+    "map.js",
+    "message.js",
+    "giveItems.js",
+    "fight.js",
+    "changeLevel.js",
+    "hud.js",
+    "xp.js",
+    "inventory.js",
+    "profile.js",
+    "playerMovement.js"
+];
+
+function loadScript(index) {
+    if (index >= scriptsFiles.length) {
+        return;
+    }
+
+    const script = document.createElement('script');
+    script.src = './JavaScript/' + scriptsFiles[index];
+    script.onload = () => loadScript(index + 1);
+    document.body.appendChild(script);
+}
+
+loadScript(0);
