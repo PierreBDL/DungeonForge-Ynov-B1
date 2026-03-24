d23add9d784af32406b99c382694b5da6b6a6db7
gihugo <hugo.giordano@ynov.com>
2026-03-24 15:43:16 +0100

    Items à rajouter

--- JavaScript/useItems.js
+++ JavaScript/useItems.js
@@ -1,14 +1,5 @@
 const inventoryCases = document.getElementById("inventoryLine");
 
-console.log(Player.inventory.push("potion_vie"))
-console.log(Player.inventory.push("potion_vie"))
-console.log(Player.inventory.push("potion_vie"))
-
-console.log(inventoryCases)
-
-console.log(Player.inventory)
-
-
 inventoryCases.addEventListener('click', (e) => {
     const caseClick = e.target.closest('.inventoryCase')
     if (!caseClick) return;
@@ -17,11 +8,13 @@ inventoryCases.addEventListener('click', (e) => {
     const img = caseClick.querySelector('img');
     const altValue = img ? img.alt : null;
 
-    console.log("Case n°", index, "— item :", altValue);
-
+    // Vérifier le nom de l'objet 
     if (altValue === 'Potion de vie') {
-        console.log("Potion de vie consommé")
         Player.inventory.splice(index, 1);
         inventory();
+        printMessage(altValue + " consommé vous regagnez 50HP.")
+        Player.stats.hp += 50;
+    } else if (altValue === '') {
+
     }
 });
