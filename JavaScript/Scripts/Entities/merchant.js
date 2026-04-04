62bd63703474c4d32190b8f3d62a2c389e4538e8
Pierre <pierre.bourgeoisdelavergne@ynov.com>
2026-04-04 16:55:38 +0200

    Extension du marchand avec 12 articles et calcul des bonus d'équipement

--- JavaScript/Scripts/Entities/merchant.js
+++ JavaScript/Scripts/Entities/merchant.js
@@ -2,7 +2,20 @@
 /*                  Marchand                    */
 /* -------------------------------------------- */
 
-function merchant (articleToSell = [{ name: "Potion de vie", price: 20}, { name: "Potion de vie", price: 20}, { name: "Potion de vie", price: 20}, { name: "Epée", price: 120}]) {
+function merchant (articleToSell = [
+    { name: "Potion de vie", price: 20}, 
+    { name: "Potion de poison", price: 35}, 
+    { name: "Potion de force", price: 50}, 
+    { name: "Potion d'armure", price: 50},
+    { name: "Épée", price: 120},
+    { name: "Épée Longue", price: 200},
+    { name: "Hache", price: 180},
+    { name: "Cuirasse", price: 150},
+    { name: "Armure Lourde", price: 280},
+    { name: "Robe Magique", price: 200},
+    { name: "Bague de Force", price: 100},
+    { name: "Amulette de Défense", price: 100}
+]) {
     // Afficher le texte du marchand
     openMerchant();
 
@@ -80,12 +93,57 @@ function buyItem (name, price) {
     } else {
         // Donner l'article en fonction de la valeur (nom du produit)
         switch(name) {
+            // Potions
             case "Potion de vie":
                 Player.inventory.push("potion_vie");
                 break;
-            case "Epée":
+            case "Potion de poison":
+                Player.inventory.push("potion_poison");
+                break;
+            case "Potion de force":
+                Player.inventory.push("potion_force");
+                break;
+            case "Potion d'armure":
+                Player.inventory.push("potion_armure");
+                break;
+            
+            // Armes
+            case "Épée":
                 Player.equipment.weapon = "epee";
-                Player.stats.attack += 10; // Augmenter l'attaque
+                Player.stats.attack = Player.stats.attackBase + 10;
+                break;
+            case "Épée Longue":
+                Player.equipment.weapon = "epee_longue";
+                Player.stats.attack = Player.stats.attackBase + 18;
+                break;
+            case "Hache":
+                Player.equipment.weapon = "hache";
+                Player.stats.attack = Player.stats.attackBase + 15;
+                break;
+            
+            // Armures
+            case "Cuirasse":
+                Player.equipment.armor = "cuirasse";
+                Player.stats.defense = Player.stats.defenseBase + 6;
+                break;
+            case "Armure Lourde":
+                Player.equipment.armor = "armure_lourde";
+                Player.stats.defense = Player.stats.defenseBase + 12;
+                break;
+            case "Robe Magique":
+                Player.equipment.armor = "robe_magique";
+                Player.stats.defense = Player.stats.defenseBase + 8;
+                Player.stats.attack += 5;
+                break;
+            
+            // Accessoires
+            case "Bague de Force":
+                Player.equipment.accessory = "bague_force";
+                Player.stats.attack += 12;
+                break;
+            case "Amulette de Défense":
+                Player.equipment.accessory = "amulette_defense";
+                Player.stats.defense += 10;
                 break;
         }
 
